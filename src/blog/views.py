42a1f4a7ff89938d047975b06f19bf7a7ee9551d
Zafir Ahmad <ahmadzafir01@gmail.com>
2019-08-14 07:31:22 +0530

    Post model for blog app is created

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2018'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28, 2018'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
