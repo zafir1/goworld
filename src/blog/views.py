19dbcb227172a24e453c866cacfcba6d4ccbac05
Zafir Ahmad <ahmadzafir01@gmail.com>
2019-08-15 07:35:54 +0530

    Blog module is working correctly.

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,22 +2,6 @@ from django.shortcuts import render
 from .models import Post
 from django.http import HttpResponse
 
-# posts = [
-#     {
-#         'author': 'CoreyMS',
-#         'title': 'Blog Post 1',
-#         'content': 'First post content',
-#         'date_posted': 'August 27, 2018'
-#     },
-#     {
-#         'author': 'Jane Doe',
-#         'title': 'Blog Post 2',
-#         'content': 'Second post content',
-#         'date_posted': 'August 28, 2018'
-#     }
-# ]
-
-
 def home(request):
     context = {
         'posts': Post.objects.all()
