3c7386457278aeeaaa6a480dfc4f49a35b23d893
Zafir Ahmad <ahmadzafir01@gmail.com>
2019-08-17 15:50:01 +0530

    Blog-post create, update, delete completed.

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
-from django.http import HttpResponse
 
 def home(request):
     context = {
@@ -13,8 +13,39 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
+    ordering = ['-date_posted']
 
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
 
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def test_func(self):
+        post = self.get_object()
+        if (self.request.user == post.author):
+            return True
+        else:
+            return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if (self.request.user == post.author):
+            return True
+        else:
+            return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
